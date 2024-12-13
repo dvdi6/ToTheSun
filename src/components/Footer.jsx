7e16a55f5440e6fe35df2d7ebd92eca73877c525
dvdi6 <online.vdiest@outlook.com>
2024-12-13 15:20:29 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div>
+        <div className="footer">
             <footer><p>© 2024 <a className="footer-link"href="https://tothesun.netlify.app/">To The Sun</a>, a React Project</p></footer>
         </div>
     )

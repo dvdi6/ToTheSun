ae5b7988e2c46a428e844420824e6b4acebf4266
dvdi6 <online.vdiest@outlook.com>
2025-01-02 13:44:37 +0100

    redirects to dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // This ensures relative paths for assets
-})
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: '_redirects', dest: 'dist' } // Copy _redirects to dist
+      ],
+      hook: 'writeBundle', // Ensures it runs after the build
+    }),
+  ],
+  base: './', // Ensures relative paths for assets
+});
